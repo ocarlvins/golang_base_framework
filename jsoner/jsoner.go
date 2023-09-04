4dc0431ee96a987c4b92dca32df9f1fb92a82614
ocarlvins <ocarlvins@gmail.com>
2023-09-04 14:51:02 +0300

    Returning a json response

--- jsoner/jsoner.go
+++ jsoner/jsoner.go
@@ -1,10 +1,36 @@
 package jsoner
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
+type person struct {
+	FirstName  string `json:"first_name"`
+	MiddleName string `json:"middle_name"`
+	LastName   string `json:"last_anme"`
+	Age        int    `json:"age"`
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func Jsoner(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "JSON Returned")
+	// fmt.Fprint(w, "JSON Returned")
+	carl := person{
+		FirstName:  "FName",
+		MiddleName: "Mname",
+		LastName:   "LName",
+		Age:        45,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	// json.NewEncoder(w).Encode(carl)
+
+	u, err := json.Marshal(carl)
+	check(err)
+	w.Write(u)
 }
