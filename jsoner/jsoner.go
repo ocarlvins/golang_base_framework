b8b82f66cf38e5c9afbc79bfa148afead48d570c
ocarlvins <ocarlvins@gmail.com>
2023-09-04 16:44:13 +0300

    Return an array of objects in json format

--- jsoner/jsoner.go
+++ jsoner/jsoner.go
@@ -1,21 +1,20 @@
 package jsoner
 
 import (
-	"fmt"
 	"net/http"
 	"webserver/structs"
 	"webserver/utilities"
 )
 
 func JsonEndpoint(w http.ResponseWriter, _ *http.Request) {
-	carl := structs.Person{
-		FirstName:  "FName",
-		MiddleName: "Mname",
-		LastName:   "LName",
-		Age:        45,
+	people := []structs.Person{
+		{FirstName: "J", MiddleName: "F", LastName: "Kennedy", Age: 75},
+		{FirstName: "Barrack", MiddleName: "H", LastName: "Obama", Age: 65},
+		{FirstName: "George", MiddleName: "W", LastName: "Bush", Age: 55},
 	}
+	//carl := structs.Person
 	// json.NewEncoder(w).Encode(carl)
-	fmt.Println(carl.ToJson())
-	fmt.Println(carl.Insert())
-	utilities.JsonResponse(w, carl)
+	//fmt.Println(carl.ToJson())
+	//fmt.Println(carl.Insert())
+	utilities.JsonResponse(w, people)
 }
