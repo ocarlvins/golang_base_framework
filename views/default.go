8901dcf8e54607cba9259620395678747e279b7b
ocarlvins <ocarlvins@gmail.com>
2023-09-07 09:45:43 +0300

    Looping through list in the go template

--- views/default.go
+++ views/default.go
@@ -8,12 +8,14 @@ import (
 func DefaultPage(w http.ResponseWriter, _ *http.Request) {
 	// Define the data to pass to the template
 	data := struct {
-		Title   string
-		Message string
+		Title     string
+		Message   string
+		FruitList []string
 	}{
-		Title:   "Hello, Template!",
-		Message: "This is a dynamic message from Go!",
+		Title:     "Hello, Template!",
+		Message:   "This is a dynamic message from Go!",
+		FruitList: []string{"Apple", "Banana", "Cherry", "Date"},
 	}
 
-	utilities.TemplateResponse("templates/index.html", data, w)
+	utilities.TemplateResponse("templates/index.gohtml", data, w)
 }
