d32869683f2bc729e6e29dd86e1cfbc26c9149f7
ocarlvins <ocarlvins@gmail.com>
2023-09-11 09:20:51 +0300

    Testing not null check

--- views/jsoner.go
+++ views/jsoner.go
@@ -12,7 +12,7 @@ func JsonEndpoint(w http.ResponseWriter, _ *http.Request) {
 	people := []structs.Person{
 		{FirstName: "J", MiddleName: "F", LastName: "Kennedy", Age: 75},
 		{FirstName: "Barrack", MiddleName: "H", LastName: "Obama", Age: 65},
-		{FirstName: "George", MiddleName: "W", LastName: "Bush", Age: 55},
+		{FirstName: "George", MiddleName: "W", Age: 55},
 	}
 	//carl := structs.Person
 	// json.NewEncoder(w).Encode(carl)
